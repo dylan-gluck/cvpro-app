e3f3c2354e2295bdd7c2da7722a345e4cf2a921e
Dylan Navajas Gluck <dylan.n.gluck@gmail.com>
2024-08-15 15:07:03 -0400

    Updated profile page layout

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,23 @@
 <script>
 	import Page from '$lib/components/layout/Page.svelte';
+	import * as Card from '$lib/components/ui/card';
+	import { Button } from '$lib/components/ui/button';
 </script>
 
 <svelte:head>
 	<title>Profile - CV Pro</title>
 </svelte:head>
 
-<Page />
+<Page>
+	<Card.Root>
+		<Card.Header>
+			<Card.Title>Basic info</Card.Title>
+			<Card.Description>Maecenas faucibus mollis ipsum dolor.</Card.Description>
+		</Card.Header>
+		<Card.Content></Card.Content>
+		<Card.Footer class="justify-end gap-2">
+			<Button size="sm" variant="secondary" disabled>Cancel</Button>
+			<Button size="sm" disabled>Save changes</Button>
+		</Card.Footer>
+	</Card.Root>
+</Page>
