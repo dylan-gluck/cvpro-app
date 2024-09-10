7c78021440977010a968af479ffb62732b8640f7
Dylan Navajas Gluck <dylan.n.gluck@gmail.com>
2024-09-10 13:06:50 -0400

    Updated layout & colors

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,7 +2,7 @@
 	import * as Card from '$lib/components/ui/card';
 	import * as Resizable from '$lib/components/ui/resizable/index.js';
 	import { Button } from '$lib/components/ui/button';
-	import { Pencil } from 'lucide-svelte';
+	import { ArrowBigRight, Pencil } from 'lucide-svelte';
 	import Page from '$lib/components/layout/Page.svelte';
 	import CardPreview from '$lib/components/profile/CardPreview.svelte';
 	import Experience from '$lib/components/profile/Experience.svelte';
@@ -10,6 +10,7 @@
 	import Education from '$lib/components/profile/Education.svelte';
 	import ResumePreview from '$lib/components/profile/ResumePreview.svelte';
 	import Certifications from '$lib/components/profile/Certifications.svelte';
+	import { ArrowRight } from 'svelte-radix';
 </script>
 
 <svelte:head>
@@ -26,14 +27,14 @@
 	</div>
 
 	<Resizable.PaneGroup direction="horizontal" class="mb-10 w-full gap-5">
-		<Resizable.Pane defaultSize={70} class="flex flex-col gap-5">
+		<Resizable.Pane defaultSize={60} class="flex flex-col gap-5">
 			<ContactInfo />
 			<Experience />
 			<Education />
 			<Certifications />
 		</Resizable.Pane>
 		<Resizable.Handle withHandle />
-		<Resizable.Pane defaultSize={30} class="flex flex-col gap-5">
+		<Resizable.Pane defaultSize={40} class="flex flex-col gap-5">
 			<Card.Root>
 				<Card.Header>
 					<Card.Title>Live Preview</Card.Title>
@@ -66,7 +67,7 @@
 				</div>
 				<div class="flex gap-2">
 					<Button size="sm" variant="outline">Back to Dashboard</Button>
-					<Button size="sm" variant="invert">Layout & Design</Button>
+					<Button size="sm" class="gap-2">Layout & Design <ArrowRight class="h-4 w-auto" /></Button>
 				</div>
 			</div>
 		</Resizable.Pane>
